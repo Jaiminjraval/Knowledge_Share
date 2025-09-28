c21a22d0bba803c55d0acf7d5eca0c69644cf4d8
Jaiminjraval <jayminraval2042006@gmail.com>
2025-09-28 16:04:55 +0530

    create core models (Subject, SessionRequest)

---
+++ KnowledgeShare/KnowledgeShare/Models/SessionRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowledgeShare.Models
+{
+    public enum RequestStatus { Pending, Approved, Declined }
+    public class SessionRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string LearnerId { get; set; }
+
+        [Required]
+        public string TutorId { get; set; }
+
+        public RequestStatus Status { get; set; } = RequestStatus.Pending;
+    }
+}
---
+++ KnowledgeShare/KnowledgeShare/Models/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowledgeShare.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string TutorId { get; set; } // Foreign key to ApplicationUser Id
+    }
+}
