a6bf81bf2317f0e49ecb1c944008ea4d2cf14526
Jaiminjraval <jayminraval2042006@gmail.com>
2025-10-07 16:37:05 +0530

    Build tutor discovery page with subject search functionality

---
+++ KnowledgeShare/KnowledgeShare/Controllers/TutorsController.cs
@@ -0,0 +1,42 @@
+﻿using KnowledgeShare.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeShare.Controllers
+{
+    public class TutorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TutorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tutors
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var tutorsQuery = _context.Users.Where(u => u.Role == "Tutor");
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var tutorIdsWithSubject = await _context.Subjects
+                    .Where(s => s.Name.ToLower().Contains(searchString.ToLower()))
+                    .Select(s => s.TutorId)
+                    .Distinct()
+                    .ToListAsync();
+
+                tutorsQuery = tutorsQuery.Where(t => tutorIdsWithSubject.Contains(t.Id));
+            }
+
+            var tutors = await tutorsQuery.ToListAsync();
+
+            // Pass all subjects to the view for efficient lookup
+            ViewBag.Subjects = await _context.Subjects.ToListAsync();
+
+            return View(tutors);
+        }
+    }
+}
