941171834fa5812e5de0f50957a85803d7257321
Jaiminjraval <jayminraval2042006@gmail.com>
2025-10-15 18:28:38 +0530

    Some parts modified 2

--- KnowledgeShare/KnowledgeShare/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ KnowledgeShare/KnowledgeShare/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,129 +1,3 @@
-// Before adding dropdown list for roles:
-
-
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations;
-//using System.Linq;
-//using System.Text;
-//using System.Text.Encodings.Web;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Authentication;
-//using Microsoft.AspNetCore.Authorization;
-//using KnowledgeShare.Data; // Ensure this is using your Data folder
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI.Services;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-//using Microsoft.AspNetCore.WebUtilities;
-//using Microsoft.Extensions.Logging;
-
-//namespace KnowledgeShare.Areas.Identity.Pages.Account
-//{
-//    [AllowAnonymous]
-//    public class RegisterModel : PageModel
-//    {
-//        private readonly SignInManager<ApplicationUser> _signInManager;
-//        private readonly UserManager<ApplicationUser> _userManager;
-//        private readonly ILogger<RegisterModel> _logger;
-//        private readonly IEmailSender _emailSender;
-
-//        public RegisterModel(
-//            UserManager<ApplicationUser> userManager,
-//            SignInManager<ApplicationUser> signInManager,
-//            ILogger<RegisterModel> logger,
-//            IEmailSender emailSender)
-//        {
-//            _userManager = userManager;
-//            _signInManager = signInManager;
-//            _logger = logger;
-//            _emailSender = emailSender;
-//        }
-
-//        [BindProperty]
-//        public InputModel Input { get; set; }
-
-//        public string ReturnUrl { get; set; }
-
-//        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-//        public class InputModel
-//        {
-//            // ADD THIS NAME PROPERTY
-//            [Required]
-//            [Display(Name = "Name")]
-//            public string Name { get; set; }
-
-//            [Required]
-//            [EmailAddress]
-//            [Display(Name = "Email")]
-//            public string Email { get; set; }
-
-//            // ADD THIS ROLE PROPERTY
-//            [Required]
-//            [Display(Name = "Role")]
-//            public string Role { get; set; }
-
-//            [Required]
-//            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-//            [DataType(DataType.Password)]
-//            [Display(Name = "Password")]
-//            public string Password { get; set; }
-
-//            [DataType(DataType.Password)]
-//            [Display(Name = "Confirm password")]
-//            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-//            public string ConfirmPassword { get; set; }
-//        }
-
-//        public async Task OnGetAsync(string returnUrl = null)
-//        {
-//            ReturnUrl = returnUrl;
-//            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-//        }
-
-//        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-//        {
-//            returnUrl = returnUrl ?? Url.Content("~/");
-//            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-//            if (ModelState.IsValid)
-//            {
-//                // UPDATE THIS USER CREATION LOGIC
-//                var user = new ApplicationUser
-//                {
-//                    UserName = Input.Email,
-//                    Email = Input.Email,
-//                    Name = Input.Name, // Add this line
-//                    Role = Input.Role    // Add this line
-//                };
-
-//                var result = await _userManager.CreateAsync(user, Input.Password);
-//                if (result.Succeeded)
-//                {
-//                    _logger.LogInformation("User created a new account with password.");
-
-//                    // This part is for email confirmation, which we have disabled for simplicity.
-//                    // You can re-enable it in a real-world application.
-
-//                    await _signInManager.SignInAsync(user, isPersistent: false);
-//                    return LocalRedirect(returnUrl);
-//                }
-//                foreach (var error in result.Errors)
-//                {
-//                    ModelState.AddModelError(string.Empty, error.Description);
-//                }
-//            }
-
-//            // If we got this far, something failed, redisplay form
-//            return Page();
-//        }
-//    }
-//}
-
-
-
-
-
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -163,7 +37,6 @@ namespace KnowledgeShare.Areas.Identity.Pages.Account
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        // --- NEW PROPERTY TO HOLD THE LIST OF ROLES ---
         public List<SelectListItem> Roles { get; set; }
 
         public class InputModel
@@ -177,7 +50,6 @@ namespace KnowledgeShare.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            // --- ADD VALIDATION TO THE ROLE PROPERTY ---
             [Required]
             [Display(Name = "Role")]
             public string Role { get; set; }
@@ -199,7 +71,6 @@ namespace KnowledgeShare.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
-            // --- POPULATE THE LIST OF ROLES WHEN THE PAGE LOADS ---
             Roles = new List<SelectListItem>
             {
                 new SelectListItem { Value = "Learner", Text = "Learner" },
